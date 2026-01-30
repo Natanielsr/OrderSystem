18c02ec5d55c77b0a284e93eb804072cbb957c43
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 11:14:59 -0300

    refactoring create order handler

--- OrderSystem.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ OrderSystem.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -15,11 +15,35 @@ public class CreateOrderHandler(
 {
     public async Task<CreateOrderResponseDto> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
     {
-        CreateOrderDto createOrderDto = request.createOrderDto;
+        Order order = mapper.Map<Order>(request);
+        order.SetNewEntity();
 
-        Order order = mapper.Map<Order>(createOrderDto);
+        order = await addProducts(request.Products, order);
 
-        foreach (var productDto in createOrderDto.Products)
+        Order createdOrder = (Order)await orderUnitOfWork.orderRepository.Add(order);
+        var success = await orderUnitOfWork.CommitAsync();
+
+        if (!success)
+            throw new Exception("Error processing the order");
+
+        return new CreateOrderResponseDto()
+        {
+            UserId = order.Id,
+            OrderId = createdOrder.Id,
+            ProductOrderResponseDtos = createdOrder.ProductsOrder.Select(
+                p => new ProductOrderResponseDto()
+                {
+                    ProductId = p.ProductId,
+                    Quantity = p.Quantity,
+                    UnitPrice = p.UnitPrice
+                }
+            ).ToList()
+        };
+    }
+
+    private async Task<Order> addProducts(List<ProductOrderDto> productOrderDtos, Order order)
+    {
+        foreach (var productDto in productOrderDtos)
         {
             Product product = (Product)await orderUnitOfWork.productRepository.GetById(productDto.ProductId);
             if (product is null)
@@ -29,6 +53,7 @@ public class CreateOrderHandler(
             {
                 ProductId = product.Id,
                 Quantity = productDto.Quantity,
+                UnitPrice = product.Price
             };
 
             order.AddProductOrder(productOrder);
@@ -36,12 +61,6 @@ public class CreateOrderHandler(
 
         }
 
-        Guid OrderId = await orderUnitOfWork.orderRepository.Add(order);
-        var success = await orderUnitOfWork.CommitAsync();
-
-        if (!success)
-            throw new Exception("Error processing the order");
-
-        return new CreateOrderResponseDto() { OrderId = OrderId };
+        return order;
     }
 }
