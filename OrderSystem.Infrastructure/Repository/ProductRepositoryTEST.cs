b9b2a410ed5db882a608e0bacea1d4fa74f06dfb
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 11:51:28 -0300

    add name to product repository test

--- OrderSystem.Infrastructure/Repository/ProductRepositoryTEST.cs
+++ OrderSystem.Infrastructure/Repository/ProductRepositoryTEST.cs
@@ -6,8 +6,16 @@ namespace OrderSystem.Infrastructure.Repository;
 
 public class ProductRepositoryTEST : IProductRepository
 {
+    List<Product> products = new List<Product>()
+    {
+        new (){ Id= Guid.NewGuid(), Name = "Monitor", Price = 1, AvailableQuantity = 1},
+        new (){ Id= Guid.NewGuid(), Name = "Teclado", Price = 2, AvailableQuantity = 2},
+        new (){ Id= Guid.NewGuid(), Name = "Mouse", Price = 3, AvailableQuantity = 3},
+    };
+
     public async Task<Entity> Add(Entity entity)
     {
+        products.Add((Product)entity);
         return entity;
     }
 
@@ -16,13 +24,13 @@ public class ProductRepositoryTEST : IProductRepository
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<Entity>> GetAll()
+    public async Task<IEnumerable<Entity>> GetAll()
     {
-        throw new NotImplementedException();
+        return products;
     }
 
-    public Task<Entity> GetById(Guid Id)
+    public async Task<Entity> GetById(Guid Id)
     {
-        throw new NotImplementedException();
+        return products.FirstOrDefault(p => p.Id == Id)!;
     }
 }
