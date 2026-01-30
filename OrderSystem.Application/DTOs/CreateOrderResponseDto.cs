18d487e29acd4fc6f92de803fb61e881e4e5b90a
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:10:27 -0300

    change id and create order product response dto in create order response dto

--- OrderSystem.Application/DTOs/CreateOrderResponseDto.cs
+++ OrderSystem.Application/DTOs/CreateOrderResponseDto.cs
@@ -2,7 +2,7 @@ namespace OrderSystem.Application.DTOs;
 
 public record class CreateOrderResponseDto
 {
-    public Guid OrderId { get; set; }
+    public Guid Id { get; set; }
     public Guid UserId { get; set; }
-    public required List<ProductOrderResponseDto> ProductOrderResponseDtos;
+    public required List<CreateOrderProductResponseDto> OrderProducts;
 }
