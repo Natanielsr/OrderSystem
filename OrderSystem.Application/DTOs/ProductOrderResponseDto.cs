41ec5e8bd70674ee1b6c467e6dfc51cfdf7227de
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 11:13:04 -0300

    create productOrderResponseDto

--- OrderSystem.Application/DTOs/ProductOrderResponseDto.cs
+++ OrderSystem.Application/DTOs/ProductOrderResponseDto.cs
@@ -1,8 +1,8 @@
 namespace OrderSystem.Application.DTOs;
 
-public record class ProductOrderDto
+public record class ProductOrderResponseDto
 {
     public required Guid ProductId { get; set; }
     public required int Quantity { get; set; }
-
+    public required decimal UnitPrice { get; set; }
 }
