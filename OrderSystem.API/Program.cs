3a28264be4244632a488a9ddef8daf6a392a9923
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 11:09:39 -0300

    add validator, mediatr injections in api program

--- OrderSystem.API/Program.cs
+++ OrderSystem.API/Program.cs
@@ -1,6 +1,9 @@
-using AutoMapper;
+
+using FluentValidation;
+using OrderSystem.Application.Orders.Commands.CreateOrder;
 
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,9 +11,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+// Registra todos os validators do assembly
+builder.Services.AddValidatorsFromAssembly(assembly);
+
 // Isso fará o AutoMapper procurar por qualquer classe que herde de 'Profile' 
 // no assembly onde o seu MappingProfile (ou qualquer classe da Application) está.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Configura o MediatR e adiciona o Behavior
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(assembly);
+    cfg.AddOpenBehavior(typeof(CreateOrderValidationBehavior<,>));
+});
 
 var app = builder.Build();
 
