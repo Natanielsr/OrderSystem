96fff74aa805bc47ce74cbe226bbddfab5ce4268
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 11:12:05 -0300

    add userid and product order response to create order response dto

--- OrderSystem.Application/DTOs/CreateOrderResponseDto.cs
+++ OrderSystem.Application/DTOs/CreateOrderResponseDto.cs
@@ -3,4 +3,6 @@ namespace OrderSystem.Application.DTOs;
 public record class CreateOrderResponseDto
 {
     public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public required List<ProductOrderResponseDto> ProductOrderResponseDtos;
 }
