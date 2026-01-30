f67653a57d43c8495a8166145350d90aefd5bad0
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:10:37 -0300

    Order dto

---
+++ OrderSystem.Application/DTOs/OrderDto.cs
@@ -0,0 +1,14 @@
+namespace OrderSystem.Application.DTOs;
+
+public record class OrderDto
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreationDate { get; set; }
+    public DateTimeOffset UpdateDate { get; set; }
+    public List<OrderProductDto>? OrderProducts { get; set; }
+    public Guid UserId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string UserEmail { get; set; } = string.Empty;
+    public bool Active { get; set; }
+
+}
