0f656760d4f4f4c4a91e6fdb84dc226ec7c100d5
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:14:13 -0300

    get order by id handler

--- OrderSystem.Application/Orders/Queries/GetOrderById/GetOrderByIdHandler.cs
+++ OrderSystem.Application/Orders/Queries/GetOrderById/GetOrderByIdHandler.cs
@@ -1,8 +1,19 @@
 using System;
+using AutoMapper;
+using MediatR;
+using OrderSystem.Application.DTOs;
+using OrderSystem.Domain.Entities;
+using OrderSystem.Domain.Repository;
 
 namespace OrderSystem.Application.Orders.Queries.GetOrderById;
 
-public class GetOrderByIdHandler
+public class GetOrderByIdHandler(IOrderRepository orderRepository, IMapper mapper) : IRequestHandler<GetOrderByIdQuery, OrderDto>
 {
+    public async Task<OrderDto> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+    {
+        Order order = (Order)await orderRepository.GetByIdAsync(request.Id);
+        OrderDto orderDto = mapper.Map<OrderDto>(order);
 
+        return orderDto;
+    }
 }
