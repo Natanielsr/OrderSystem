9da7f50b0b76777cba4ff97624f7864bcf4c9e05
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:11:29 -0300

    mapping order profile

--- OrderSystem.Application/Mappings/OrderMappingProfile.cs
+++ OrderSystem.Application/Mappings/OrderMappingProfile.cs
@@ -1,4 +1,3 @@
-using System;
 using AutoMapper;
 using OrderSystem.Application.DTOs;
 using OrderSystem.Application.Orders.Commands.CreateOrder;
@@ -10,12 +9,13 @@ public class OrderMappingProfile : Profile
 {
     public OrderMappingProfile()
     {
+        CreateMap<OrderProduct, CreateOrderProductResponseDto>();
         CreateMap<Order, CreateOrderResponseDto>();
 
         CreateMap<CreateOrderCommand, Order>()
-            .ForMember(
-                dest => dest.ProductsOrder,
-                opt => opt.Ignore()
-            );
+            .ForMember(dest => dest.OrderProducts, opt => opt.Ignore());
+
+        CreateMap<Order, OrderDto>();
+        CreateMap<OrderProduct, OrderProductDto>();
     }
 }
