fbde7eb082aa3094d320a50a02fbc8d342fd82f6
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:15:48 -0300

    create order validator

--- OrderSystem.Application/Validator/CreateOrderValidator.cs
+++ OrderSystem.Application/Validator/CreateOrderValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using OrderSystem.Application.DTOs;
 using OrderSystem.Application.Orders.Commands.CreateOrder;
 
 namespace OrderSystem.Application.Validator;
@@ -7,14 +8,24 @@ public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
 {
     public CreateOrderValidator()
     {
-        RuleFor(o => o.Products)
+        RuleFor(o => o.OrderProducts)
             .NotNull().WithMessage("product list can´t be null")
             .NotEmpty().WithMessage("product list can´t be empty");
 
-        RuleForEach(o => o.Products)
+        RuleFor(o => o.OrderProducts)
+            .Must(HasNoDuplicates)
+            .WithMessage("the list contains duplicate product IDs.");
+
+        RuleForEach(o => o.OrderProducts)
             .Must(p => p.Quantity > 0).WithMessage("Product quantity must be bigger then zero");
 
-        RuleForEach(o => o.Products)
+        RuleForEach(o => o.OrderProducts)
             .Must(p => p.ProductId != Guid.Empty).WithMessage("ProductId can´t be empty");
+    }
+
+    private bool HasNoDuplicates(List<CreateOrderProductDto> list)
+    {
+        // Compara o total de itens com o total de IDs únicos
+        return list.Select(x => x.ProductId).Distinct().Count() == list.Count;
     }
 }
