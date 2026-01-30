a3bb92c1cfdcb1e360ce52c5db775e2bf9e88d06
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-01-30 16:11:14 -0300

    remove productorderresponsedto

--- OrderSystem.Application/DTOs/ProductOrderResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace OrderSystem.Application.DTOs;
-
-public record class ProductOrderResponseDto
-{
-    public required Guid ProductId { get; set; }
-    public required int Quantity { get; set; }
-    public required decimal UnitPrice { get; set; }
-}
