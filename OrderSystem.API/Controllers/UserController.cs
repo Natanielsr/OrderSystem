5c143544d5c4f366a62458a8255b64795e41f2a8
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-03 16:04:57 -0300

    create user controller

---
+++ OrderSystem.API/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderSystem.Application.DTOs.User;
+using OrderSystem.Application.Users.Commands.Auth;
+using OrderSystem.Application.Users.Commands.CreateUser;
+
+namespace OrderSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand createUserCommand)
+        {
+            CreateUserResponseDto response = await mediator.Send(createUserCommand);
+            return CreatedAtRoute("GetUserById", new { id = response.Id }, response);
+        }
+
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] AuthCommand login)
+        {
+            var response = await mediator.Send(login);
+            return Ok(response);
+        }
+
+
+        [HttpGet("{id:guid}", Name = "GetUserById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            // var response = await mediator.Send(new GetOrderByIdQuery(id));
+
+            return Ok();
+        }
+
+
+    }
+}
