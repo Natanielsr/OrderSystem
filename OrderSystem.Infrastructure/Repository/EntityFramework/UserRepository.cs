a09743c80c8c41d80005732601f58714c8bf514c
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-03 16:13:39 -0300

    Add get by username and email in user repository

--- OrderSystem.Infrastructure/Repository/EntityFramework/UserRepository.cs
+++ OrderSystem.Infrastructure/Repository/EntityFramework/UserRepository.cs
@@ -27,9 +27,27 @@ public class UserRepository(AppDbContext context) : IUserRepository
         return false;
     }
 
+    public async Task<User> GeByUserNameAsync(string username)
+    {
+        var user = await context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Username == username);
+
+        return user!;
+    }
+
     public async Task<IEnumerable<Entity>> GetAllAsync()
     {
         return await context.Users.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<User> GetByEmailAsync(string email)
+    {
+        var user = await context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == email);
+
+        return user!;
     }
 
     public async Task<Entity> GetByIdAsync(Guid id)
