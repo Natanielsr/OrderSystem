3c4e9be0db5dce965f75e97647fe050a1dee3337
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-03 16:07:34 -0300

    user mapping

---
+++ OrderSystem.Application/Mappings/UserMappingProfile.cs
@@ -0,0 +1,17 @@
+using System;
+using AutoMapper;
+using OrderSystem.Application.DTOs.User;
+using OrderSystem.Application.Users.Commands.CreateUser;
+using OrderSystem.Domain.Entities;
+
+namespace OrderSystem.Application.Mappings;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<CreateUserCommand, User>();
+        CreateMap<User, CreateUserResponseDto>();
+        CreateMap<User, AuthResponseDto>();
+    }
+}
