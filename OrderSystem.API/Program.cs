6eb78a0007e4fe2fae49c1ef877f1df2816e6e3b
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-04 11:01:52 -0300

    add auth token in swagger

--- OrderSystem.API/Program.cs
+++ OrderSystem.API/Program.cs
@@ -1,9 +1,11 @@
 
+using Microsoft.OpenApi.Models;
 using OrderSystem.API.Filters;
 using OrderSystem.Application.DependencyInjection;
 using OrderSystem.Domain.Services;
 using OrderSystem.Infrastructure;
 using OrderSystem.Infrastructure.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
@@ -11,7 +13,33 @@ var assembly = typeof(Program).Assembly;
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddControllers(options =>
 {
@@ -44,6 +72,7 @@ using (var scope = app.Services.CreateScope())
 }
 // --- FIM DO BLOCO DE SEED ---
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -51,7 +80,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 
