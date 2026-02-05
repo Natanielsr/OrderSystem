c5ee75dc788818faf6028bd7bab3e6042bedc6e8
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-05 10:42:17 -0300

    create product handler

---
+++ OrderSystem.Application/Products/Commands/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using AutoMapper;
+using MediatR;
+using OrderSystem.Application.DTOs.Product;
+using OrderSystem.Application.Services;
+using OrderSystem.Domain.Entities;
+using OrderSystem.Domain.Repository;
+using OrderSystem.Domain.UnitOfWork;
+
+namespace OrderSystem.Application.Products.Commands.CreateProduct;
+
+public class CreateProductHandler(
+    IProductRepository productRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper,
+    IStorageService storageService
+    ) : IRequestHandler<CreateProductCommand, CreateProductResponseDto>
+{
+    public async Task<CreateProductResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        Product product = mapper.Map<Product>(request);
+        product.SetDefaultEntityProps();
+
+        string filePath = await storageService.UploadFileAsync(request.FileStream, request.FileName);
+        product.ImagePath = filePath;
+
+        var createdProduct = await productRepository.AddAsync(product);
+        var response = mapper.Map<CreateProductResponseDto>(createdProduct);
+
+        await unitOfWork.CommitAsync();
+
+        return response;
+    }
+}
