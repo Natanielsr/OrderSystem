e53dfe9443644f282aaacbeb18d648e4b379d46f
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-05 10:41:56 -0300

    product mapping

---
+++ OrderSystem.Application/Mappings/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+using System;
+using AutoMapper;
+using OrderSystem.Application.DTOs.Product;
+using OrderSystem.Application.Products.Commands.CreateProduct;
+using OrderSystem.Domain.Entities;
+
+namespace OrderSystem.Application.Mappings;
+
+public class ProductMappingProfile : Profile
+{
+    public ProductMappingProfile()
+    {
+        CreateMap<CreateProductCommand, Product>();
+        CreateMap<Product, CreateProductResponseDto>();
+    }
+}
