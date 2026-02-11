18c5ebfe7a332e39d8b8e7add838df413584be9d
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-11 10:53:44 -0300

    auth with email

--- OrderSystem.API/Controllers/AuthController.cs
+++ OrderSystem.API/Controllers/AuthController.cs
@@ -1,7 +1,7 @@
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OrderSystem.Application.Users.Commands.Auth;
+using OrderSystem.Application.Users.Commands.AuthEmail;
 
 namespace OrderSystem.API.Controllers
 {
@@ -11,6 +11,13 @@ namespace OrderSystem.API.Controllers
     {
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] AuthCommand login)
+        {
+            var response = await mediator.Send(login);
+            return Ok(response);
+        }
+
+        [HttpPost("LoginEmail")]
+        public async Task<IActionResult> LoginEmail([FromBody] AuthEmailCommand login)
         {
             var response = await mediator.Send(login);
             return Ok(response);
