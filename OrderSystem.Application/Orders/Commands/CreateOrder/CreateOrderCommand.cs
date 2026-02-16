33f58a4a8033d3116462c37c889cf91454ca09b3
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-16 15:52:53 -0300

    createordercommand with paymentmethod

--- OrderSystem.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ OrderSystem.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,9 +1,14 @@
 using MediatR;
 using OrderSystem.Application.DTOs.Order;
+using OrderSystem.Domain.Entities;
 
 namespace OrderSystem.Application.Orders.Commands.CreateOrder;
 
-public record class CreateOrderCommand(List<CreateOrderProductDto> OrderProducts, Guid UserId) : IRequest<CreateOrderResponseDto>
+public record class CreateOrderCommand(
+    List<CreateOrderProductDto> OrderProducts,
+    Guid UserId,
+    PaymentMethod PaymentMethod
+    ) : IRequest<CreateOrderResponseDto>
 {
 
 }
