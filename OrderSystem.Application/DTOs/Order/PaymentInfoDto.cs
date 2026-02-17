cbf120e12f42c8bac9118b7574753af3935d8881
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-17 10:20:50 -0300

    retornar o id no payment info

--- OrderSystem.Application/DTOs/Order/PaymentInfoDto.cs
+++ OrderSystem.Application/DTOs/Order/PaymentInfoDto.cs
@@ -4,6 +4,7 @@ namespace OrderSystem.Application.DTOs.Order;
 
 public record class PaymentInfoDto
 {
+    public Guid Id { get; set; }
     public PaymentMethod Method { get; set; } // "CreditCard", "Pix", "Boleto"
     public decimal PaidAmount { get; set; }
     public string TransactionReference { get; set; } = string.Empty;
