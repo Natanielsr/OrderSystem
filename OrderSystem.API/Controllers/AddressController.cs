f8d97997e49169977c31cc9e0374192b16236730
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-19 13:21:36 -0300

    Get Address by Id  controller

--- OrderSystem.API/Controllers/AddressController.cs
+++ OrderSystem.API/Controllers/AddressController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrderSystem.API.Security;
 using OrderSystem.Application.Addresses.Commands.CreateAddress;
+using OrderSystem.Application.Addresses.Queries.GetAddressById;
 using OrderSystem.Application.Authorization;
 using OrderSystem.Application.DTOs.Address;
 
@@ -26,6 +27,24 @@ namespace OrderSystem.API.Controllers
             AddressDto response = await mediator.Send(createAddressCommand);
 
             return CreatedAtRoute("GetAddressById", new { id = response.Id }, response);
+        }
+
+        [Authorize]
+        [HttpGet("{id:guid}", Name = "GetAddressById")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            AddressDto? response = await mediator.Send(new GetAddressByIdQuery(id));
+            if (response == null)
+                return NotFound("Address Not Found");
+
+            var userClaim = APIClaim.createUserClaim(User);
+            var authResponse = AuthorizationBase.ValidUser(userClaim, response.UserId);
+            if (!authResponse.Success)
+            {
+                return StatusCode(403, authResponse.Message);
+            }
+
+            return Ok(response);
         }
     }
 }
