480d69207a4f36f2cbd010129cc2a0839ef3d697
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-19 11:21:51 -0300

    create addres command

---
+++ OrderSystem.Application/Addresses/Commands/CreateAddress/CreateAddressCommand.cs
@@ -0,0 +1,19 @@
+using MediatR;
+using OrderSystem.Application.DTOs.Address;
+
+namespace OrderSystem.Application.Addresses.Commands.CreateAddress;
+
+public record class CreateAddressCommand(
+    string FullName,
+    string Cpf,
+    string Street,
+    string Number,
+    string Complement,
+    string Neighborhood,
+    string City,
+    string State,
+    string ZipCode
+) : IRequest<AddressDto>
+{
+
+}
