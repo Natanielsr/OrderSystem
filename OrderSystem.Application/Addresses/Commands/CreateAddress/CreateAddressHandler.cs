b8c71da2415fb54c3ba0709900f4151eadc89c89
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-19 13:56:41 -0300

    commit unit of work in create address handler

--- OrderSystem.Application/Addresses/Commands/CreateAddress/CreateAddressHandler.cs
+++ OrderSystem.Application/Addresses/Commands/CreateAddress/CreateAddressHandler.cs
@@ -4,16 +4,18 @@ using MediatR;
 using OrderSystem.Application.DTOs.Address;
 using OrderSystem.Domain.Entities;
 using OrderSystem.Domain.Repository;
+using OrderSystem.Domain.UnitOfWork;
 
 namespace OrderSystem.Application.Addresses.Commands.CreateAddress;
 
-public class CreateAddressHandler(IAddressRepository repository, IMapper mapper) : IRequestHandler<CreateAddressCommand, AddressDto>
+public class CreateAddressHandler(IAddressRepository repository, IMapper mapper, IUnitOfWork unitOfWork) : IRequestHandler<CreateAddressCommand, AddressDto>
 {
     public async Task<AddressDto> Handle(CreateAddressCommand request, CancellationToken cancellationToken)
     {
         Address address = mapper.Map<Address>(request);
 
         var response = await repository.AddAsync(address);
+        await unitOfWork.CommitAsync();
 
         AddressDto addressDto = mapper.Map<AddressDto>(response);
 
