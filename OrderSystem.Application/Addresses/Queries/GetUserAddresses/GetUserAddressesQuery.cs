e7a3f514f476236e3e195c54cc77add61d15c368
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-25 11:26:27 -0300

    remove page from get user addresses query

--- OrderSystem.Application/Addresses/Queries/GetUserAddresses/GetUserAddressesQuery.cs
+++ OrderSystem.Application/Addresses/Queries/GetUserAddresses/GetUserAddressesQuery.cs
@@ -4,7 +4,7 @@ using OrderSystem.Domain.Entities;
 
 namespace OrderSystem.Application.Addresses.Queries.GetUserAddresses;
 
-public record class GetUserAddressesQuery(Guid UserId, int Page, int PageSize) : IRequest<List<AddressDto>>
+public record class GetUserAddressesQuery(Guid UserId) : IRequest<List<AddressDto>>
 {
 
 }
