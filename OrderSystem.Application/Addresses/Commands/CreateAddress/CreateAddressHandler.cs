108aa28d443cebae28a1648a4d4bbe7b4ff5a71b
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-25 11:07:05 -0300

    verify user address if have 5 and set default

--- OrderSystem.Application/Addresses/Commands/CreateAddress/CreateAddressHandler.cs
+++ OrderSystem.Application/Addresses/Commands/CreateAddress/CreateAddressHandler.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using MediatR;
 using OrderSystem.Application.DTOs.Address;
 using OrderSystem.Domain.Entities;
+using OrderSystem.Domain.Exceptions;
 using OrderSystem.Domain.Repository;
 using OrderSystem.Domain.UnitOfWork;
 
@@ -12,9 +13,21 @@ public class CreateAddressHandler(IAddressRepository repository, IMapper mapper,
 {
     public async Task<AddressDto> Handle(CreateAddressCommand request, CancellationToken cancellationToken)
     {
-        Address address = mapper.Map<Address>(request);
+        Address newAddress = mapper.Map<Address>(request);
 
-        var response = await repository.AddAsync(address);
+        var userAddresses = await repository.GetUserAddressesAsync(request.UserId, 1, 5);
+        if (userAddresses.Count() > 5)
+            throw new AddressCountExceededException();
+
+        if (request.IsDefault)
+        {
+            foreach (var address in userAddresses)
+            {
+                address.IsDefault = false;
+            }
+        }
+
+        var response = await repository.AddAsync(newAddress);
         await unitOfWork.CommitAsync();
 
         AddressDto addressDto = mapper.Map<AddressDto>(response);
