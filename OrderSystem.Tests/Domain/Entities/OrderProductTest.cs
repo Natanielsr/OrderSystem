3b219a4223ffbc30e993445e9c0a1daee2f9cdd1
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-26 09:28:13 -0300

    change create OrderProduct test

--- OrderSystem.Tests/Domain/Entities/OrderProductTest.cs
+++ OrderSystem.Tests/Domain/Entities/OrderProductTest.cs
@@ -9,7 +9,18 @@ public class OrderProductTest
     public void TestTotal()
     {
         //Arrange
-        OrderProduct orderProduct = new(Guid.NewGuid(), "TestName", 10m, 10);
+        OrderProduct orderProduct = new OrderProduct()
+        {
+            Id = Guid.NewGuid(),
+            CreationDate = DateTimeOffset.UtcNow,
+            UpdateDate = DateTimeOffset.UtcNow,
+            Active = true,
+            OrderId = Guid.Empty,
+            ProductId = Guid.Empty,
+            ProductName = "product.Name",
+            UnitPrice = 10,
+            Quantity = 10
+        };
 
         //Act
         var total = orderProduct.Total;
