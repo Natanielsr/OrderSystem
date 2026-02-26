99c2ad536229f1df6862e3210660eb955417cdf3
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-26 09:42:09 -0300

    change order map to active true

--- OrderSystem.Application/Mappings/OrderMappingProfile.cs
+++ OrderSystem.Application/Mappings/OrderMappingProfile.cs
@@ -16,7 +16,7 @@ public class OrderMappingProfile : Profile
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
             .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => DateTimeOffset.UtcNow))
             .ForMember(dest => dest.UpdateDate, opt => opt.MapFrom(src => DateTimeOffset.UtcNow))
-            .ForMember(dest => dest.Active, opt => opt.MapFrom(src => DateTimeOffset.UtcNow))
+            .ForMember(dest => dest.Active, opt => opt.MapFrom(src => true))
             .ForMember(dest => dest.OrderProducts, opt => opt.Ignore());
 
         CreateMap<Order, OrderDto>();
