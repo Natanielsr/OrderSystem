c026a44556cc100e2b13e83bd27f23cd19596252
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-26 09:25:36 -0300

    remove seed from on model creating in app db context

--- OrderSystem.Infrastructure/Data/AppDbContext.cs
+++ OrderSystem.Infrastructure/Data/AppDbContext.cs
@@ -22,14 +22,6 @@ public class AppDbContext : DbContext
             .Property(p => p.Version)
             .IsConcurrencyToken(); // O EF vai usar isso no WHERE automaticamente
 
-        var p1Id = Guid.Parse("868d8763-125c-4384-8149-c12e52e50529");
-        var p2Id = Guid.Parse("671b4025-a134-45e0-9e67-0c7f1a308967");
-
-        modelBuilder.Entity<Product>().HasData(
-            new Product(p1Id, "Product1", 10m, 10),
-            new Product(p2Id, "Product2", 20m, 20)
-        );
-
     }
 
 }
