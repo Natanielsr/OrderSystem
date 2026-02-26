e4623145776a7c6268b2f81c44ff8e185b3355bc
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-26 09:22:12 -0300

    add required init props and remove constructor from payment info

--- OrderSystem.Domain/Entities/PaymentInfo.cs
+++ OrderSystem.Domain/Entities/PaymentInfo.cs
@@ -11,37 +11,20 @@ public enum PaymentStatus
 public class PaymentInfo : Entity
 {
     // Dados para o histórico (Snapshot)
-    public PaymentMethod Method { get; private set; } // "CreditCard", "Pix", "Boleto"
-    public decimal PaidAmount { get; private set; }
+    public required PaymentMethod Method { get; init; } // "CreditCard", "Pix", "Boleto"
+    public required decimal PaidAmount { get; init; }
     // Rastreabilidade Externa
-    public string TransactionReference { get; private set; } // O ID que vem do Gateway
-    public string LastFourDigits { get; private set; }       // Apenas para o cliente identificar o cartão
-    public string ProviderName { get; private set; }        // Ex: "Stripe", "Adyen"
+    public required string TransactionReference { get; init; } // O ID que vem do Gateway
+    public required string LastFourDigits { get; init; }       // Apenas para o cliente identificar o cartão
+    public required string ProviderName { get; init; }        // Ex: "Stripe", "Adyen"
 
     // Status do Pagamento (usando a dica da string que vimos antes)
-    public PaymentStatus Status { get; private set; } // "Authorized", "Captured", "Refunded", "Failed"
-
-    public Guid OrderId { get; set; }
+    public required PaymentStatus Status { get; init; } // "Authorized", "Captured", "Refunded", "Failed"
+    public required Guid OrderId { get; init; }
     public Order? Order { get; set; }
 
-    public PaymentInfo(
-        Guid Id,
-        DateTimeOffset CreationDate,
-        DateTimeOffset UpdateDate,
-        bool Active,
-        PaymentMethod Method,
-        decimal PaidAmount,
-        string TransactionReference,
-        string LastFourDigits,
-        string ProviderName,
-        PaymentStatus Status
-        ) : base(Id, CreationDate, UpdateDate, Active)
+    public PaymentInfo()
     {
-        this.Method = Method;
-        this.PaidAmount = PaidAmount;
-        this.TransactionReference = TransactionReference;
-        this.LastFourDigits = LastFourDigits;
-        this.ProviderName = ProviderName;
-        this.Status = Status;
+
     }
 }
