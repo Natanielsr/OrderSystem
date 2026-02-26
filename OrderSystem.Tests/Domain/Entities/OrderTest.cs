52dc3e40ef510ea3f82895df1151bb87d9167748
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-26 09:28:35 -0300

    change create order test obj

--- OrderSystem.Tests/Domain/Entities/OrderTest.cs
+++ OrderSystem.Tests/Domain/Entities/OrderTest.cs
@@ -9,17 +9,52 @@ public class OrderTest
     public void TotalTest()
     {
         //Arrange
-        OrderProduct orderProduct1 = new(Guid.NewGuid(), "Product1", 10.00m, 10);
-        OrderProduct orderProduct2 = new(Guid.NewGuid(), "Product2", 10.00m, 10);
+        OrderProduct orderProduct1 = createSimpleOrderProduct("Product1", 10.00m, 10);
+        OrderProduct orderProduct2 = createSimpleOrderProduct("Product2", 10.00m, 10);
 
-        Order order = new Order(Guid.NewGuid());
+        Order order = createSimpleOrder();
         order.AddProductOrder(orderProduct1);
         order.AddProductOrder(orderProduct2);
 
         //Act
-        var total = order.CalcTotal;
+        var total = Order.CalcTotal(order.OrderProducts);
 
         //Assert
         Assert.Equal(200m, total);
+    }
+
+    public Order createSimpleOrder()
+    {
+        return new Order()
+        {
+            Id = Guid.NewGuid(),
+            CreationDate = DateTimeOffset.UtcNow,
+            UpdateDate = DateTimeOffset.UtcNow,
+            Active = true,
+            OrderProducts = new List<OrderProduct>(),
+            UserId = Guid.Empty,
+            UserName = "userName",
+            UserEmail = "userEmail",
+            Total = 0,
+            Status = OrderStatus.Pending,
+            Code = "code",
+            AddressId = Guid.Empty
+        };
+    }
+
+    public OrderProduct createSimpleOrderProduct(string name, decimal price, int quantity)
+    {
+        return new OrderProduct()
+        {
+            Id = Guid.NewGuid(),
+            CreationDate = DateTimeOffset.UtcNow,
+            UpdateDate = DateTimeOffset.UtcNow,
+            Active = true,
+            OrderId = Guid.Empty,
+            ProductId = Guid.NewGuid(),
+            ProductName = name,
+            UnitPrice = price,
+            Quantity = quantity
+        };
     }
 }
